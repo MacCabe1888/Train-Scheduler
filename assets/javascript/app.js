1c4d306c334a85b87904eb2daada2494bd837269
Cameron <cameronjmaccabe@gmail.com>
2019-03-13 00:04:31 -0700

    integrated Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
+let config = {
+    apiKey: "AIzaSyBHhZDk4Uw8z-Yzn6AcxAPmVKDSKfZT9i8",
+    authDomain: "https://maccabe1888.github.io/Train-Scheduler/",
+    databaseURL: "https://train-scheduler-e9907.firebaseio.com",
+    projectID: "train-scheduler-e9907",
+    messagingSenderId: "984877380458"
+};
+
+firebase.initializeApp(config);
+
+let database = firebase.database();
+
 let trainArrival = new Audio("assets/audio/Electric-train-arriving-at-a-station-sound-effect.mp3");
 
 $("#submit").on("click", function(event) {
@@ -55,5 +67,51 @@ $("#submit").on("click", function(event) {
     }
 
     firstUpdate;
+
+    //Create local temporary object for holding train data
+	let newTrain = {
+		name: name,
+		destination: destination,
+		firstTime: firstTime,
+		frequency: frequency,
+		nextArrival: nextArrival,
+		minutesAway: minutesAway,
+		currentTime: currentTime.format("hh:mm A")
+    };
+    
+	//Save/upload train data to the database.
+    database.ref().push(newTrain);
  
+})
+
+database.ref().on("child_added", function(childSnapshot) {
+
+	//Set variables for form input field values equal to the stored values in firebase.
+	let name = childSnapshot.val().name;
+	let destination = childSnapshot.val().destination;
+	let firstTime = childSnapshot.val().firstTime;
+	let frequency = childSnapshot.val().frequency;
+	let nextArrival = childSnapshot.val().nextArrival;
+	let minutesAway = childSnapshot.val().minutesAway;
+    let currentTime = childSnapshot.val().currentTime;
+
+    let firstTimeConverted = moment(firstTime, "HH:mm").subtract(frequency, "minutes");
+    let currentTime = moment();
+    let diffInTime = moment(currentTime).diff(moment(firstTimeConverted), "minutes");
+    let timeRemainder = diffInTime % frequency;
+    let minutesAway = frequency - timeRemainder;
+    let nextArrival = moment().add(minutesAway, "minutes").format("LT");
+    let nextArrivalDisplay = $("<td>");
+    let minutesAwayDisplay = $("<td>");
+
+    let newRow = $("<tr>").append(
+        $("<td>").text(name),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        nextArrivalDisplay.text(nextArrival),
+        minutesAwayDisplay.text(minutesAway),
+    );
+
+    $("#train-schedule > tbody").append(newRow);
+    
 })
