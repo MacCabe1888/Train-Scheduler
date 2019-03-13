143b9febe2c0bed7472d6153b089e3a9e431d123
Cameron <cameronjmaccabe@gmail.com>
2019-03-13 04:09:35 -0700

    fixed Firebase integration bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,9 @@
 let config = {
     apiKey: "AIzaSyBHhZDk4Uw8z-Yzn6AcxAPmVKDSKfZT9i8",
-    authDomain: "https://maccabe1888.github.io/Train-Scheduler/",
+    authDomain: "train-scheduler-e9907.firebaseapp.com",
     databaseURL: "https://train-scheduler-e9907.firebaseio.com",
-    projectID: "train-scheduler-e9907",
+    projectId: "train-scheduler-e9907",
+    storageBucket: "train-scheduler-e9907.appspot.com",
     messagingSenderId: "984877380458"
 };
 
@@ -12,7 +13,7 @@ let database = firebase.database();
 
 let trainArrival = new Audio("assets/audio/Electric-train-arriving-at-a-station-sound-effect.mp3");
 
-$("#submit").on("click", function(event) {
+$("#submit").on("click", function() {
 
     event.preventDefault();
 
@@ -21,9 +22,33 @@ $("#submit").on("click", function(event) {
     let firstTime = $("#first-time").val().trim();
     let frequency = $("#frequency").val().trim();
 
-    let firstTimeConverted = moment(firstTime, "HH:mm").subtract(frequency, "minutes");
+    //Create local temporary object for holding train data
+	let newTrain = {
+		name: name,
+		destination: destination,
+		firstTime: firstTime,
+		frequency: frequency
+    };
+    
+	//Save/upload train data to the database.
+    database.ref().push(newTrain);
+ 
+})
+
+database.ref().on("child_added", function(childSnapshot) {
+
+ 	//Set variables for form input field values equal to the stored values in firebase.
+ 	let name = childSnapshot.val().name;
+ 	let destination = childSnapshot.val().destination;
+ 	let firstTime = childSnapshot.val().firstTime;
+ 	let frequency = childSnapshot.val().frequency;
+
     let currentTime = moment();
-    let diffInTime = moment(currentTime).diff(moment(firstTimeConverted), "minutes");
+    let diffInTime = moment(currentTime).diff(moment(firstTime, "HH:mm"), "minutes");
+    if (diffInTime < 0) {
+    let firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "days");
+    diffInTime = moment(currentTime).diff(moment(firstTimeConverted), "minutes");
+    }
     let timeRemainder = diffInTime % frequency;
     let minutesAway = frequency - timeRemainder;
     let nextArrival = moment().add(minutesAway, "minutes").format("LT");
@@ -68,50 +93,4 @@ $("#submit").on("click", function(event) {
 
     firstUpdate;
 
-    //Create local temporary object for holding train data
-	let newTrain = {
-		name: name,
-		destination: destination,
-		firstTime: firstTime,
-		frequency: frequency,
-		nextArrival: nextArrival,
-		minutesAway: minutesAway,
-		currentTime: currentTime.format("hh:mm A")
-    };
-    
-	//Save/upload train data to the database.
-    database.ref().push(newTrain);
- 
-})
-
-database.ref().on("child_added", function(childSnapshot) {
-
-	//Set variables for form input field values equal to the stored values in firebase.
-	let name = childSnapshot.val().name;
-	let destination = childSnapshot.val().destination;
-	let firstTime = childSnapshot.val().firstTime;
-	let frequency = childSnapshot.val().frequency;
-	let nextArrival = childSnapshot.val().nextArrival;
-	let minutesAway = childSnapshot.val().minutesAway;
-    let currentTime = childSnapshot.val().currentTime;
-
-    let firstTimeConverted = moment(firstTime, "HH:mm").subtract(frequency, "minutes");
-    let currentTime = moment();
-    let diffInTime = moment(currentTime).diff(moment(firstTimeConverted), "minutes");
-    let timeRemainder = diffInTime % frequency;
-    let minutesAway = frequency - timeRemainder;
-    let nextArrival = moment().add(minutesAway, "minutes").format("LT");
-    let nextArrivalDisplay = $("<td>");
-    let minutesAwayDisplay = $("<td>");
-
-    let newRow = $("<tr>").append(
-        $("<td>").text(name),
-        $("<td>").text(destination),
-        $("<td>").text(frequency),
-        nextArrivalDisplay.text(nextArrival),
-        minutesAwayDisplay.text(minutesAway),
-    );
-
-    $("#train-schedule > tbody").append(newRow);
-    
 })
